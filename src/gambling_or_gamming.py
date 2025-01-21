f0e5eb860f69ce179a434d3309e616b0e43efce5
Vinay Pepakayala <132639307+vinay-852@users.noreply.github.com>
2025-01-21 20:17:05 +0000

    changes

--- src/gambling_or_gamming.py
+++ src/gambling_or_gamming.py
@@ -18,7 +18,7 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 
 async def extract_important_gg(info):
     prompt = """
-    Your task is to analyze the provided text which is related to gambling or gaming. I will give the text from analyzing the app description, reviews, and permissions. You have to identify whether the text is related to gambling or gaming. You should classify it as gambling or gaming with reasoning. The result should be context specific, don't assume anything.
+    Your task is to analyze the provided text which is related to gambling or gaming. I will give the text from analyzing the app description, reviews, and permissions. You have to identify whether the text is related to gambling or gaming. You should classify it as gambling or gaming with reasoning. The result should be context specific, don't assume anything. Gambling with real or virtual money is always gambling, remember that.
     """
     prompt_complete = prompt + str(info)
     response = await asyncio.to_thread(model.generate_content, prompt_complete,generation_config=genai.GenerationConfig(
